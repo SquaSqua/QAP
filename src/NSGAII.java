e89bbf063a27de47aade9c70302905ae15b82b25
SquaSqua <ewaskorska94@gmail.com>
2018-05-11 07:40:51 +0200

    błąd w wyświetlaniu tablicy

--- src/NSGAII.java
+++ src/NSGAII.java
@@ -1,21 +1,26 @@
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Random;
 
 public class NSGAII {
     private int[][] distanceMatrix;
     private int[][][] matricesArray;
     private int generations;
     private int pop_size;
+    private double cross_prob;
+    private double mut_prob;
     private int flowsNumber;
     String instanceFile;
     ArrayList<Individual> population = new ArrayList<>();
     ArrayList<ArrayList<Individual>> paretoFronts = new ArrayList<>();
 
-    public NSGAII(String instanceFile, int generations, int pop_size) {
+    public NSGAII(String instanceFile, int generations, int pop_size, double cross_prob, double mut_prob) {
         this.generations = generations;
         this.pop_size = pop_size;
         this.instanceFile = instanceFile;
+        this.cross_prob = cross_prob;
+        this.mut_prob = mut_prob;
     }
 
     public void initialize() {
@@ -56,34 +61,96 @@ public class NSGAII {
         }
     }
 
-    public void frontGenerator() {
-        ArrayList<Individual> currentFront = new ArrayList<>();
-        currentFront.add(population.get(0));
-        paretoFronts.add(currentFront);
+    public String frontGenerator() {
+
+        paretoFronts.add(new ArrayList<>());
         for(int i = 0; i < population.size(); i++) {
-            for(int j = 0; j < paretoFronts.size(); j++) {
-                ArrayList<Individual> temp = paretoFronts.get(j);
-                if(temp.size() == 0) {
-                    temp.add(population.get(i));
-                }
-                else {
-                    for(int k = 0; k < temp.size(); k++) {
-                        if((population.get(i).compareTo(temp.get(k)) == 0) && (k == temp.size() - 1)) {
-                            temp.add(population.get(i));
-                        }
-                        else if(population.get(i).compareTo(temp.get(k)) == -1) {
-                            //zamiana miejsc
-                        }
-                        else if(population.get(i).compareTo(temp.get(k)) == 1) {
-                            //nowy front
+            for (int j = 0; j < paretoFronts.size(); j++) {
+                ArrayList<Individual> currentFront = paretoFronts.get(j);
+                if (currentFront.size() == 0) {
+                    currentFront.add(population.get(i));
+                    break;
+                } else {
+                    for (int k = 0; k < currentFront.size(); k++) {
+                        int compared = population.get(i).compareTo(currentFront.get(k));
+                        if ((compared == 0) && (k == currentFront.size() - 1)) {
+                            currentFront.add(population.get(i));
+                            if(i < population.size() - 1) {
+                                i++;
+                                j = -1;
+                            }else {
+                                j = paretoFronts.size();
+                            }
                             break;
+                        } else if (compared == -1) {
+                            //zamiana miejsc
+                            ArrayList<Individual> betterFront = new ArrayList<>();
+                            betterFront.add(population.get(i));
+                            for(int z = 0; z < k; ) {
+                                betterFront.add(currentFront.get(z));
+                                currentFront.remove(z);
+                                k--;
+                            }
+                            for(int z = 1; z < currentFront.size(); z++) {
+                                if(population.get(i).compareTo(currentFront.get(z)) == 0) {
+                                    betterFront.add(currentFront.get(z));
+                                    currentFront.remove(z);
+                                    z--;
+                                }
+                            }
+                            paretoFronts.add(j, betterFront);
+                            if(i < population.size() - 1) {
+                                i++;
+                                j = -1;
+                            }else {
+                                j = paretoFronts.size();
+                            }
+                            break;
+                        } else if (compared == 1) {
+                            //nowy front
+                            if (paretoFronts.size() < j + 2) {
+                                paretoFronts.add(new ArrayList<>());
+                                paretoFronts.get(j + 1).add(population.get(i));
+                                if(i < population.size() - 1) {
+                                    i++;
+                                    j = -1;
+                                }else {
+                                    j = paretoFronts.size();
+                                }
+                                break;
+                            }
+                            else {
+                                break;
+                            }
                         }
                     }
                 }
-
-
             }
+
         }
+//        for(ArrayList<Individual> a : paretoFronts) {
+//            System.out.println("*************************");
+//            for(Individual i : a) {
+//                i.toString();
+//            }
+//        }
+        return dataGenerator();
+    }
+
+    public String dataGenerator() {
+        String result = "";
+        StringBuilder sB = new StringBuilder(result);
+        for(ArrayList<Individual> a : paretoFronts) {
+            for(Individual i : a) {
+                for(int j = 0; j < i.fitnessArray.length; j++) {
+                    sB.append(i.fitnessArray[j] + ", ");
+                }
+                sB.append("\n");
+            }
+            sB.append("\n");
+        }
+        result = sB.toString();
+        return result;
     }
 //    public void putInPareto() {
 //        for(int i = 0; i < population.size(); i++) {
@@ -161,5 +228,60 @@ public class NSGAII {
 //    public ArrayList<Individual> matingPool() {
 //
 //    }
+
+    public void mutation(Individual ind) {
+        int[] permutation = ind.getPermutation();
+        int length = permutation.length;
+        Random rand = new Random();
+
+        for(int i = 0; i < length; i++) {
+            if(Math.random() < mut_prob) {
+                int index = rand.nextInt(length);
+                int temp = permutation[index];
+                permutation[index] = permutation[i];
+                permutation[i] = temp;
+            }
+        }
+        ind.setPermutation(permutation);
+    }
+
+    public Individual[] crossing_OrderedX(Individual ind1, Individual ind2)
+    {
+        Individual[] result = new Individual[2];
+        if(Math.random() < cross_prob) {
+            int[] perm1 = ind1.getPermutation();
+            int[] perm2 = ind2.getPermutation();
+            int[] res1 = new int[perm1.length];
+            int[] res2 = new int[perm1.length];
+            Random rand = new Random();
+            int first = rand.nextInt(perm1.length);
+            int second = rand.nextInt(perm1.length);
+            if(first > second) {
+                int temp = first;
+                first = second;
+                second = temp;
+            }
+            for(int i = first; i < second; i++) {
+                res1[i] = perm2[i];
+                res2[i] = perm1[i];
+            }
+            for(int i = 0; i < perm1.length; i++) {
+                if(i < first || i > second) {
+
+                }
+            }
+        }
+        else {
+            result[0] = ind1;
+            result[1] = ind2;
+        }
+        return result;
+    }
+    public Individual[] crossing_CX(Individual ind1, Individual ind2) {
+        if()
+    }
+    public Individual[] crossing_PMX(Individual ind1, Individual ind2) {
+        if()
+    }
 }
 
--- src/Runner.java
+++ src/Runner.java
@@ -1,10 +1,21 @@
+import java.io.*;
+
 public class Runner {
     public static void main(String[] args) {
         String instanceFile = "src//instances//KC10-2fl-1rl.dat";
-        NSGAII nsga2 = new NSGAII(instanceFile, 1, 15);
-
+        NSGAII nsga2 = new NSGAII(instanceFile, 1, 10,0.5, 0.5);
         nsga2.initialize();
+
+        String results = "data.csv";
+        try
+        {
+            PrintWriter out = new PrintWriter(results);
+            out.println(nsga2.frontGenerator());
+            out.close();
+        }
+        catch (FileNotFoundException ex)
+        {
+            System.out.println("Nie da się utworzyć pliku!");
+        }
     }
-
-
 }
